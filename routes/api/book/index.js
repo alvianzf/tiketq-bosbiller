64e12c49a2714e58b8cfa7fb00b3d3c08ef636fe
alvianzf <alvianzf@gmail.com>
2024-06-25 12:45:19 +0700

    feat: add book endpoint

--- routes/api/book/index.js
+++ routes/api/book/index.js
@@ -1,8 +1,14 @@
+const makeRequest = require('../../../utils/axios-request');
+
 const router = require('express').Router();
 
 router.post('/', async (req, res, next) => {
+    let requestData = req.body;
+    requestData.f = "book"
     try {
-        
+        const result = makeRequest(JSON.stringify(requestData));
+
+        return res.send(result.data);
     } catch (error) {
         next(error);
     }
