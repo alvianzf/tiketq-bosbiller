a9469c739176672f7c5b0eddfbc45b8260c1ad55
alvianzf <alvianzf@gmail.com>
2024-06-27 08:51:24 +0700

    refactor: refactor endpoint list

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,24 +2,32 @@ const express = require('express');
 const router = express.Router();
 const createError = require('http-errors');
 
-router.use('/airports', require('./airports'));
-router.use('/airlines', require('./airlines'));
-router.use('/search', require('./search'));
-router.use('/book', require('./book'));
-router.use('/book-info', require('./bookinfo'));
-router.use('/search-airport', require('./search-airport'));
+// Route imports
+const airportsRouter = require('./airports');
+const airlinesRouter = require('./airlines');
+const searchRouter = require('./search');
+const bookRouter = require('./book');
+const bookInfoRouter = require('./bookinfo');
+const searchAirportRouter = require('./search-airport');
 
+// Route configurations
+router.use('/airports', airportsRouter);
+router.use('/airlines', airlinesRouter);
+router.use('/search', searchRouter);
+router.use('/book', bookRouter);
+router.use('/book-info', bookInfoRouter);
+router.use('/search-airport', searchAirportRouter);
+
+// Root endpoint
 router.get('/', (req, res, next) => {
   try {
-    res.send({
-      data: "/api endpoint, thread carefully",
+    res.json({
+      data: "/api endpoint, tread carefully",
       error: null
     });
   } catch (err) {
     next(createError(500, err.message || 'Internal Server Error'));
   }
 });
-
-
 
 module.exports = router;
