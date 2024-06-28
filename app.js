6cdaca7d8e158089fee84f8a209c4642f8556c84
alvianzf <alvianzf@gmail.com>
2024-06-28 13:59:58 +0700

    feat: add mongodb connection

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const logger = require('morgan');
 const cors = require('cors');
 const indexRouter = require('./routes/index');
 const { notFoundHandler, errorHandler } = require('./middleware/error-handler');
+const connectDB = require('./utils/db');
+
+// db connection;
 
 const app = express();
 
@@ -17,6 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// connect to Database
+connectDB();
 
 app.use('/', indexRouter);
 
--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,17 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+
+const DB_NAME = process.env.DB_NAME;
+const DB_URI = `mongodb://localhost:27017/${DB_NAME}`;
+
+const connectDB = async () => {
+    try {
+      await mongoose.connect(DB_URI);
+      console.log('MongoDB connected');
+    } catch (err) {
+      console.error('MongoDB connection error:', err);
+      process.exit(1);
+    }
+  };
+  
+module.exports = connectDB;
