25fc7cf9443d13a13cc37e4103cb6020a4673850
alvianzf <alvianzf@gmail.com>
2024-06-28 15:20:53 +0700

    feat: add passenger username to schema

--- db/models/FlightBooking.js
+++ db/models/FlightBooking.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const toGMT7 = require('../../utils/timezone-convert');
 
 const FlightBookingSchema = new mongoose.Schema({
+  passenger_username: { type: String },
   passenger_name: { type: String, required: true },
   amount: { type: Number, required: true },
   flight_date: { type: Date, required: true },
