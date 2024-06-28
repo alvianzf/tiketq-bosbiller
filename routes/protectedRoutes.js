57124a33861594f9c6417ebb5986ae3864910b58
alvianzf <alvianzf@gmail.com>
2024-06-28 16:16:16 +0700

    deploy: disable protected payment route

--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.use('/api/payment', authMiddleware, require('./api/payment'));
+router.use('/api/payment', require('./api/payment'));
 router.use('/api/flight-bookings', authMiddleware, require('./api/flight-bookings'));
 
 module.exports = router;
