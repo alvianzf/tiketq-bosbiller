576eeda668d45f85627f750714daf3b3e97e297a
alvianzf <alvianzf@gmail.com>
2024-07-09 19:03:17 +0700

    fix: remove Bearer string from auth to authenticate JWT

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization');
+  const token = req.header('Authorization').replace('Bearer ', '');
 
   if (!token) {
     return res.status(401).json({ error: 'Authorization denied' });
