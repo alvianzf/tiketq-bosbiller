cde76b10365ed9c52457c5c104d4ee18db2e6a75
alvianzf <alvianzf@gmail.com>
2024-10-03 19:20:50 +0700

    docs: add swagger

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const { notFoundHandler, errorHandler } = require('./middleware/error-handler');
 const connectDB = require('./db');
 const routes = require('./routes');
 const protectedRoutes = require('./routes/protectedRoutes');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const app = express();
 
@@ -22,6 +25,10 @@ app.use(cookieParser());
 // Serve static files from 'public' and 'assets' directories
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 // Connect to Database
 connectDB();
