3a6578964599650cf9c3f91fd6d4ac5349e9b19c
alvianzf <alvianzf@gmail.com>
2024-10-08 18:20:42 +0700

    feat: remove validation

--- routes/api/book/index.js
+++ routes/api/book/index.js
@@ -2,28 +2,18 @@ const express = require('express');
 const apiService = require('../../../services/apiService');
 const router = express.Router();
 const FlightBookingDAO = require('../../../db/dao/FlightBookingDAO');
-const { body, validationResult } = require('express-validator');
 
 /**
  * POST request to book a flight
+ * @param {string} bookingCode - The booking code of the flight
+ * @param {number} nominal - The nominal value of the booking
  * @param {string} origin - The origin of the flight
  * @param {string} destination - The destination of the flight
  * @param {string} departureDate - The departure date of the flight
  * @param {string} mobile_number - The mobile number of the buyer
  * @param {string} name - The name of the buyer
  */
-router.post('/', [
-  body('origin').notEmpty().withMessage('Origin is required'),
-  body('destination').notEmpty().withMessage('Destination is required'),
-  body('departureDate').notEmpty().isISO8601().withMessage('Valid departure date is required'),
-  body('mobile_number').notEmpty().isMobilePhone().withMessage('Valid mobile number is required'),
-  body('name').notEmpty().withMessage('Name is required')
-], async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
+router.post('/', async (req, res, next) => {
   const requestData = { ...req.body, f: "book" };
 
   try {
