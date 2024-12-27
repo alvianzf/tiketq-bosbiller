dac0e78e3b84002bd8a14e1c9bb83232317cc558
alvianzf <alvianzf@gmail.com>
2024-12-27 10:08:33 +0700

    feat: add token on middleware

--- middleware/ensure-token.js
+++ middleware/ensure-token.js
@@ -1,26 +1,26 @@
-const NodeCache = require('node-cache');
+const NodeCache = require("node-cache");
 const cache = new NodeCache({ stdTTL: 3600, checkperiod: 600 });
 
 async function ensureToken(req, res, next) {
-  const token = cache.get('ferryToken');
-  
-  if(!token) {
+  const token = cache.get("ferryToken");
+
+  if (!token) {
     try {
       const response = await axios.post(OAUT2_TOKEN_PATH, {
         name: FERRY_USERNAME,
-        password: FERRY_PASSWORD
+        password: FERRY_PASSWORD,
       });
 
-      cache.set('ferryToken', response.data, 3600);
+      cache.set("ferryToken", response.data, 3600);
+      req.token = response.data;
       next();
     } catch (error) {
-      return res.status(401).json({ message: 'Unauthorized' });
+      return res.status(401).json({ message: "Unauthorized" });
     }
   }
 
   req.token = token;
   next();
 }
-
 
 module.exports = ensureToken;
