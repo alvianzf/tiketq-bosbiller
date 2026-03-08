6f1059a9423577146e6b00b866404f8b3af14677
alvianzf <alvianzf@gmail.com>
2026-03-08 16:04:10 +0700

    refactor: update protected routes to point to relocated flight bookings

--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -1,21 +1,25 @@
 /**
  * Importing required modules and utilities.
  */
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require("../middleware/authMiddleware");
 
 /**
  * Mounting the payment API routes at the '/api/payment' path.
  */
-router.use('/api/payment', require('./api/payment'));
+router.use("/api/payment", require("./api/payment"));
 
 /**
- * Mounting the flight bookings API routes at the '/api/flight-bookings' path.
- * 
+ * Mounting the flight bookings API routes at the '/api/flight/bookings' path.
+ *
  * @param {Function} authMiddleware - The middleware function to authenticate the user.
  */
-router.use('/api/flight-bookings', authMiddleware, require('./api/flight-bookings'));
+router.use(
+  "/api/flight/bookings",
+  authMiddleware,
+  require("./api/flight-bookings"),
+);
 
 /**
  * Exporting the router instance to be used in the application.
